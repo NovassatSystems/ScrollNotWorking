7c12a1e327f4a102ffee21ba5a71c11bf02e71aa
Douglas Simao <douglas.simao.dev@gmail.com>
2023-06-24 01:46:13 +0100

    Add project files.

---
+++ ScrollNotWorking/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ScrollNotWorking;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ ScrollNotWorking/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ScrollNotWorking;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
